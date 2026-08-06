0b93da258681419327209477ad2f19de20e94d34
Saahil Lia <121372698+Saahillia@users.noreply.github.com>
2026-08-06 20:34:50 +0530

    Time: 1 ms (100%), Space: 42.9 MB (14.66%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -1,16 +1,13 @@
 class Solution {
 
-    // Helper method to calculate the product of digits of a given number
     private int getDigitProduct(int number) {
         if (number == 0) {
-            return 0; // The product of digits for 0 is 0
+            return 0; 
         }
         int product = 1;
         int temp = number;
         while (temp > 0) {
             int digit = temp % 10;
-            // If any digit is 0, the entire product will be 0.
-            // This is important because 0 is divisible by any non-zero t.
             if (digit == 0) {
                 return 0;
             }
@@ -22,13 +19,12 @@ class Solution {
 
     public int smallestNumber(int n, int t) {
         int num = n;
-        // Loop indefinitely, incrementing num, until the condition is met
         while (true) {
             int digitProduct = getDigitProduct(num);
             if (digitProduct % t == 0) {
-                return num; // Found the smallest number satisfying the condition
+                return num;
             }
-            num++; // Move to the next number
+            num++;
         }
     }
 }
