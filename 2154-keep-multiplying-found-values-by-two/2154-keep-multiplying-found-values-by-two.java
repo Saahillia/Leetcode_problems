2a9c2adb2eb67c1051902e8a8a34ad467f17eb46
Saahil Lia <121372698+Saahillia@users.noreply.github.com>
2026-08-10 18:11:46 +0530

    Time: 0 ms (100%), Space: 45.6 MB (83.8%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
@@ -1,16 +1,11 @@
 class Solution {
     public int findFinalValue(int[] nums, int original) {
-        // Convert nums to a HashSet for O(1) average time complexity lookups
-        Set<Integer> numSet = new HashSet<>();
-        for (int num : nums) {
-            numSet.add(num);
-        }
 
-        // Repeatedly double original as long as it exists in the set
-        while (numSet.contains(original)) {
-            original *= 2;
+        for(int i=0; i<nums.length; i++){
+            if(original == nums[i]){
+                return findFinalValue(nums,original*2);
+            }
         }
-
         return original;
     }
 }
